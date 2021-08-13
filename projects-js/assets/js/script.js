71d0b9900687b7f56f09042f8f80896e89424fd9
sandriansyafri <sandriannsyafri@gmail.com>
2021-08-13 09:28:37 +0700

    finished

--- projects-js/assets/js/script.js
+++ projects-js/assets/js/script.js
@@ -1,1 +1,14 @@
-console.log(`External : Hello World!`);
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan(porsi) {
+        this.energi = this.energi + porsi;
+        return `energi kamu sekarang adalah ${this.energi}`;
+    }
+}
+
+const sandrian = new Mahasiswa("sandrian", 100);
+const hafid = new Mahasiswa("hafid", 200);
