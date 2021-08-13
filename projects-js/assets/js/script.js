3deaf400331f7ea230c9ff83fb7e3a61ea98ef0d
sandriansyafri <sandriannsyafri@gmail.com>
2021-08-13 11:57:47 +0700

    finished

--- projects-js/assets/js/script.js
+++ projects-js/assets/js/script.js
@@ -1,1 +1,15 @@
 console.log(`External : Hello World!`);
+const box = document.querySelector(".box");
+box.addEventListener("click", function () {
+    let size = "size";
+    let caption = "caption";
+
+    if (this.classList.contains(size)) {
+        [size, caption] = [caption, size];
+    }
+
+    this.classList.toggle(size);
+    setTimeout(() => {
+        this.classList.toggle(caption);
+    }, 600);
+});
