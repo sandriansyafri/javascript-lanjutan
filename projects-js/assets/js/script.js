1b9f976eefe30986d6839e70e94ef085223f6f39
sandriansyafri <sandriannsyafri@gmail.com>
2021-08-13 11:41:57 +0700

    finished

--- projects-js/assets/js/script.js
+++ projects-js/assets/js/script.js
@@ -1,1 +1,38 @@
-console.log(`External : Hello World!`);
+//* =======================================
+//* CONSTRUCTOR FUNCTION ==================
+//* =======================================
+
+const students = function (nama) {
+    this.nama = nama;
+    this.sayHello = function () {
+        console.log(`Hello ${this.nama}`);
+    };
+};
+const sandrian = new students("sandrian");
+//TODO : diable comment to run script
+// console.log(sandrian);
+
+//* =======================================
+//* ARROW FUNCTION ========================
+//* =======================================
+
+//TODO disable comment to run script
+// const persons = function (nama) {
+//     this.nama = nama;
+//     this.count = 0;
+
+//     setInterval(() => {                 /** penerapan this pada arrow function */
+//         console.log(++this.count);
+//     }, 1000);
+// };
+
+const persons = function (nama) {
+    this.nama = nama;
+    this.count = 0;
+
+    setInterval(function () {
+        console.log(this); /** function deklaration dihoisting sehingga this mengarah pada object window */
+    }, 1000);
+};
+//TODO : diable comment to run script
+// const fikri = new persons("nama");
