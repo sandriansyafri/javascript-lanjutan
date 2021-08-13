70aba5ff04d14f00efdc890ef211e5589bd02bad
sandriansyafri <sandriannsyafri@gmail.com>
2021-08-13 10:31:53 +0700

    finished

--- projects-js/assets/js/script.js
+++ projects-js/assets/js/script.js
@@ -1,1 +1,45 @@
-console.log(`External : Hello World!`);
+//* =======================================
+//* LATIHAN -1 ============================
+//* =======================================
+
+function init() {
+    let nama = "sandrian"; /* Local Variabel */
+    return function () {
+        /* Closure */
+        console.log(nama);
+    };
+}
+
+// const myClosure = init();
+
+//* =======================================
+//* LATIHAN -2 ============================
+//* =======================================
+
+const ucapkanSalam = function (waktu) {
+    return function (nama) {
+        console.log(`Halo ${nama} selamat ${waktu}`);
+    };
+};
+
+const selamatPagi = ucapkanSalam("pagi");
+const selamatSiang = ucapkanSalam("siang");
+const selamatSore = ucapkanSalam("sore");
+const selamatMalam = ucapkanSalam("malam");
+
+// selamatMalam("fakri");
+
+// *=======================================
+// * LATIHAN -3 ============================
+// *=======================================
+
+let add = (function () {
+    let counter = 0; /* local variable */
+    return function () {
+        return ++counter;
+    };
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
