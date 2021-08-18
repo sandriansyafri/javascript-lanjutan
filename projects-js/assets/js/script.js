addcc5f9ab2ed688492e8633c86437e0e2f05377
sandriannsyafri <sandriannsyafri@gmail.com>
2021-08-18 07:53:54 +0700

    template-literal

--- projects-js/assets/js/script.js
+++ projects-js/assets/js/script.js
@@ -1,1 +1,23 @@
-console.log(`External : Hello World!`);
+//* =========================================
+//* TEMPLATE STRINGS ========================
+//* =========================================
+
+//* STRING 
+const firstName = 'Sandrian';
+const lastName = 'Syafri';
+
+const fullName = `${firstName} ${lastName}`;
+const sayHi = `Hello ${fullName}`;
+// console.log(sayHi);
+
+//* EXPRESSION
+const x = 10;
+const y = 20;
+const result = `${x + y == 30 ? 'x + y = 30' : ''}`;
+
+//* HTML FRAGMENT
+const message = 'Hello World';
+const el = `<div>
+    <h1>${message}</h1>
+</div>`;
+// document.body.innerHTML = el;
