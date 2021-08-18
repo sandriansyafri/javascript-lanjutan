302e339fb13a87c3ffd2aac0e6a0f922d6fa2fd7
sandriannsyafri <sandriannsyafri@gmail.com>
2021-08-18 13:35:58 +0700

    for of dan for in

--- projects-js/assets/js/script.js
+++ projects-js/assets/js/script.js
@@ -1,1 +1,66 @@
-console.log(`External : Hello World!`);
+//*=======================================================
+//*FOR OF  ===============================================
+//*=======================================================
+
+/**
+ * *Iterable Object
+ *  ? STRING
+ *  ? ARRAY
+ *  ? Argument
+ *  ? Nodelist
+ *  ? Map
+ *  ? Set
+ *  ? User Defined Variable
+ *
+ */
+
+//* For of
+// const students = ["a", "b", "c", "d", "e", "f", "g"];
+// for (const [i, student] of students.entries()) {
+//   console.log(`ini adalah ${student} dengan index ke ${i}`);
+// }
+
+//* String
+// const fullName = "Sandrian Syafri";
+// for (string of fullName) {
+//   console.log(string);
+// }
+
+//* NodeList
+// const lists = document.querySelectorAll("ul li");
+// for (list of lists) {
+//   console.log(list.textContent);
+// }
+
+//* Arguments
+// const items = function (...items) {
+//   // TODO diable
+//   // return Array.from(arguments).reduce((a, i) => a + i, 0);
+//   // return items.reduce((a, i) => a + i, 0);
+//   //* ----------------- for of -------------------------------
+//   // TODO diable
+//   //   let totals = 0;
+//   //   for (argument of arguments) {
+//   //     totals += argument;
+//   //   }
+//   //   return totals;
+// };
+
+// const result = items(1, 2, 3, 5, 6);
+// console.log(result);
+
+//*=======================================================
+//* FOR IN  ==============================================
+//*=======================================================
+
+//* For in
+// TODO diable
+// const students = {
+//   name: "Sandrian",
+//   age: 23,
+//   address: "abc",
+// };
+
+// for (student in students) {
+//   console.log(students[student]);
+// }
