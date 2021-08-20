a758b6af10d4b78e03f05d166fab6a3c4ec925eb
sandriannsyafri <sandriannsyafri@gmail.com>
2021-08-20 07:18:30 +0700

    fetch

--- projects-js/assets/js/script.js
+++ projects-js/assets/js/script.js
@@ -1,29 +1,89 @@
-const students = [
-  { id: 1, name: "a" },
-  { id: 2, name: "b" },
-  { id: 3, name: "c" },
-];
+const btnSearch = document.querySelector("button.btn-search");
 
-const studies = [
-  { id: 1, name: "d" },
-  { id: 2, name: "e" },
-  { id: 3, name: "f" },
-];
+const showCard = function (movie) {
+  return `<div class="col-lg-4">
+                <div class="card my-2">
+                    <div class="img-wrapper">
+                        <img src="${movie.Poster}" alt=" " class="mb-3" />
+                    </div>
+                <div class="card-body">
+                    <div class="card-title">
+                    <h5>${movie.Title}</h5>
+                    </div>
+                    <div class="card-subtitle">
+                    <p>${movie.Year}</p>
+                    </div>
+                </div>
+                <div class="card-footer bg-white">
+                    <button
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                    type="button"
+                    data-imdbid=${movie.imdbID}
+                    class="btn btn-primary btn-md btn-detail"
+                    >
+                    Detail
+                    </button>
+                </div>
+                </div>
+            </div>`;
+};
 
-const getStudents = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve(students);
-  }, 4000);
+const showDetailCard = function (movie) {
+  return `<div class="container-fluid">
+                <div class="row">
+                    <div class="col-4 border">
+                    <img src="${movie.Poster}" class="img-fluid" alt="" />
+                    </div>
+                    <div class="col">
+                    <ul class="list-group">
+                        <li class="list-group-item"> <b>Title :</b> <span>${movie.Title}</span></li>
+                        <li class="list-group-item"> <b>Year :</b> <span>${movie.Year}</span></li>
+                        <li class="list-group-item"> <b>Rate :</b> <span>${movie.Rated}</span></li>
+                        <li class="list-group-item"> <b>Plot :</b> <div>${movie.Plot}</div></li>
+                    </ul>
+                    </div>
+                </div>
+                </div>`;
+};
+
+const getMovies = function (keywords) {
+  return fetch(`http://www.omdbapi.com/?apikey=2b9a984b&s=${keywords}`)
+    .then((res) => res.json())
+    .then((res) => res.Search);
+};
+
+const getDetailMovies = function (id) {
+  return fetch(`http://www.omdbapi.com/?apikey=2b9a984b&i=${id}`)
+    .then((res) => res.json())
+    .then((res) => res);
+};
+
+const updateUI = function (movies) {
+  let card = "";
+  const movieContainer = document.querySelector(".movie-container");
+  movies.map((movie) => (card += showCard(movie)));
+  movieContainer.innerHTML = card;
+};
+
+const updateDetailUI = function (movie) {
+  const modalBody = document.querySelector(".modal-body");
+  const movieDetail = showDetailCard(movie);
+  modalBody.innerHTML = movieDetail;
+};
+
+btnSearch.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const keywords = document.querySelector("input[name=keywords]").value;
+  const movies = await getMovies(keywords);
+  updateUI(movies);
 });
 
-const getStudies = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve(studies);
-  }, 1000);
-});
-
-Promise.all([getStudents, getStudies]).then((res) => {
-  const [students, studies] = res;
-  console.log(studies);
-  console.log(students);
+document.addEventListener("click", async function (e) {
+  const target = e.target;
+  if (target.classList.contains("btn-detail")) {
+    const id = target.dataset.imdbid;
+    const movie = await getDetailMovies(id);
+    updateDetailUI(movie);
+  }
 });
